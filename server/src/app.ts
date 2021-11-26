19e3ca2d65a21ba258b4bb2b9fd2180cd16ec75b
Maira Fidalgo <mairaberlatof@gmail.com>
2021-11-26 18:03:16 -0300

    Routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import cors from 'cors';
+import routes from './routes';
 import './database/connection';
 
-const port = 3333;
+const port = 3000;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+app.use(routes);
 
 app.listen(port);
